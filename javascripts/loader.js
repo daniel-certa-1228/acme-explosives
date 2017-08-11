318cba7795723d2dfc00723d8195bc407523b7e0
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-11 10:33:12 -0500

    Resolved promises

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -28,6 +28,35 @@ bombLoader.loadCategories = () => {
 		categoryLoader.addEventListener("load", (event) => {
 			let categories = JSON.parse(event.target.responseText);
 			resolve(categories);
+			bombCategories.push(categories);
+		});
+	});
+};
+
+bombLoader.loadTypes = () => {
+	return new Promise(function(resolve,reject){
+		let typeLoader = new XMLHttpRequest();
+		typeLoader.open("GET", "json/types.json");
+		typeLoader.send();
+
+		typeLoader.addEventListener("load", (event) => {
+			let types = JSON.parse(event.target.responseText);
+			resolve(types);
+			bombTypes.push(types);
+		});
+	});
+};
+
+bombLoader.loadProducts = () => {
+	return new Promise(function(resolve, reject){
+		let productLoader = new XMLHttpRequest();
+		productLoader.open("GET", "json/products.json");
+		productLoader.send();
+
+		productLoader.addEventListener("load", (event) => {
+			let products = JSON.parse(event.target.responseText);
+			resolve(products);
+			bombProducts.push(products);
 		});
 	});
 };
